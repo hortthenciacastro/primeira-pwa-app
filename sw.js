0a8b4f4f5a303483b985e82efa4b6d3f27de62ec
hortthenciacastro <hortthencia.castro@estudante.ifms.edu.br>
2025-08-29 15:06:29 -0400

    término

---
+++ sw.js
@@ -0,0 +1,20 @@
+let  cacheName = "my-firts-pwa";
+let filesToCache = ["/", "/index.html",
+            "/css/styles.css", "/js/main.js"];
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache)
+    {
+        return cache.addAll(filesToCache);
+    })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
